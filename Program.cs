4cefc97d3f988357ad41cf9c5e87f239b33b3e26
Abhinav <codedreamer06@gmail.com>
2021-10-31 18:04:06 +0530

    Create db

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Data.SQLite;
+
+namespace DotNet_SQLite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          if (File.Exists(@"abhinavDb.sqlite")) {
+            Console.WriteLine("abhinavDb already exists.");
+          }
+
+          else {
+            Console.WriteLine("Creating abhinavDb.sqlite");
+            SQLiteConnection.CreateFile("abhinavDb.sqlite");
+          }
+        }
+    }
+}
